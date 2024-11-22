0ce3f4dc831ae41944226801fc00e570268d5e0c
Tamás Vince Kornél <vince@tvk.lol>
2024-11-22 09:18:02 +0100

    discord: refactor command loading

--- src/discord/index.ts
+++ src/discord/index.ts
@@ -4,9 +4,15 @@ import * as unlink from "./commands/unlink";
 import * as username from "./commands/username";
 import * as auth from "./commands/authorize";
 import { config } from "@/utils/config";
-import { REST, Routes, Client, EmbedBuilder, type Interaction } from "discord.js";
+import { REST, Routes, Client, EmbedBuilder, type Interaction, Collection } from "discord.js";
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from 'url';
 
-export const commands = {
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
+
+const commands = {
   ping,
   link,
   unlink,
@@ -14,10 +20,11 @@ export const commands = {
   auth
 };
 
+
 export class DiscordBot {
   public client: Client;
   private rest: REST;
-  private commandsData: unknown[];
+  private commandsData: Collection<string, unknown> = new Collection();
 
   constructor() {
     this.client = new Client({
@@ -25,14 +32,30 @@ export class DiscordBot {
     });
 
     this.rest = new REST().setToken(config.discord.botToken);
-    this.commandsData = Object.values(commands).map((command) => command.data);
+
+    this.commandsData = new Collection();
 
     this.client.once("ready", this.onReady.bind(this));
     this.client.on("interactionCreate", this.onInteractionCreate.bind(this));
   }
 
+  private async loadCommands() {
+    const foldersPath = path.join(__dirname, 'commands');
+    const commandFiles = fs.readdirSync(foldersPath);
+    for (const file of commandFiles) {
+      const filePath = path.join(foldersPath, file);
+      const command = await import(filePath);
+      if ('data' in command && 'execute' in command) {
+        console.log(`[INFO] Loaded command ${command.data.name} from ${filePath}`);
+        this.commandsData.set(command.data.name, command);
+      } else {
+        console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+      }
+    }
+  }
+
   private async onReady() {
-    console.log("Ready!");
+    console.log("Discord bot ready!");
   }
 
   private async onInteractionCreate(interaction: Interaction) {
@@ -49,11 +72,12 @@ export class DiscordBot {
 
   public async start() {
     try {
+      await this.loadCommands();
       console.log("Started refreshing application (/) commands.");
-
       await this.rest.put(
         Routes.applicationGuildCommands(config.discord.appId, config.discord.guildId),
-        { body: this.commandsData },
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        { body: this.commandsData.map((command: any) => command.data.toJSON()) }
       );
 
       console.log("Successfully reloaded application (/) commands.");
